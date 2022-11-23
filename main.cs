70326a3a44bf2692f0325d4e94a7795f3c6316cc
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-11-23 19:27:57 +0000

    whitespace

--- main.cs
+++ main.cs
@@ -1,7 +1,8 @@
 using System;
 
-class Program {
-  public static void Main(string[] args)
+class Program
+{
+    public static void Main(string[] args)
     {
         int age;
 
@@ -11,17 +12,21 @@ class Program {
         Console.Write("Enter your age: ");
         age = Convert.ToInt32(Console.ReadLine());
 
-        if (age >= 17) {
-          Console.WriteLine("You can watch an R rated movie alone!");
+        if (age >= 17)
+        {
+            Console.WriteLine("You can watch an R rated movie alone!");
         }
-        else if (age >= 13) {
-          Console.WriteLine("You can watch a PG-13 rated movie alone! ");
+        else if (age >= 13)
+        {
+            Console.WriteLine("You can watch a PG-13 rated movie alone! ");
         }
-        else if (age >= 5) {
-          Console.WriteLine("You can watch a PG rated movie alone! ");
+        else if (age >= 5)
+        {
+            Console.WriteLine("You can watch a PG rated movie alone! ");
         }
-        else {
-          Console.WriteLine("You're too young to do most things! ");
+        else
+        {
+            Console.WriteLine("You're too young to do most things! ");
         }
 
         Console.WriteLine("\nDone.");
